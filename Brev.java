46a1dfd4ba88f61d80a048e2631e1f8e029896f7
benjaminbylund <31540367+benjaminbylund@users.noreply.github.com>
2017-10-03 17:31:57 +0200

    Update Brev.java

--- Brev.java
+++ Brev.java
@@ -3,13 +3,12 @@ import javax.swing.*;
 /*  Programmet berättar om brevet är giltigt 
 	Benjamin Bylund
 	2017-10-02
-	140<langd>=600
 */
 public class Brev {
 	public static void main(String[] args) {
-		int langd = Integer.parseInt(JOptionPane.showInputDialog(null, "Ge mig langd")); //max 600, min 140
-		int bred = Integer.parseInt(JOptionPane.showInputDialog(null, "Ge mig bred")); //double bred,
-		int tjocklek = Integer.parseInt(JOptionPane.showInputDialog(null, "Ge mig tjcoklek")); //double tjocklek
+		int langd = Integer.parseInt(JOptionPane.showInputDialog(null, "Ge mig langd")); 
+		int bred = Integer.parseInt(JOptionPane.showInputDialog(null, "Ge mig bred")); 
+		int tjocklek = Integer.parseInt(JOptionPane.showInputDialog(null, "Ge mig tjcoklek")); 
 
 		if (((langd > 140) && (langd < 600)) && 
 			(tjocklek < 100) && 
