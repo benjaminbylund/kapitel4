4ce53fdb5521897c9eb3a3aab66b95df990098dd
Benjamin Bylund <bybe16@nti.umea>
2017-10-03 15:48:24 +0200

    Uppgift 4.4

---
+++ Brev.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+
+/*  Programmet berättar om brevet är giltigt 
+	Benjamin Bylund
+	2017-10-02
+	140<langd>=600
+*/
+public class Brev {
+	public static void main(String[] args) {
+		int langd = Integer.parseInt(JOptionPane.showInputDialog(null, "Ge mig langd")); //max 600, min 140
+		int bred = Integer.parseInt(JOptionPane.showInputDialog(null, "Ge mig bred")); //double bred,
+		int tjocklek = Integer.parseInt(JOptionPane.showInputDialog(null, "Ge mig tjcoklek")); //double tjocklek
+
+		if (((langd > 140) && (langd < 600)) && 
+			(tjocklek < 100) && 
+			((bred > 90) && (bred < 200))){
+			JOptionPane.showMessageDialog(null, "OK!");
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "FEL!");
+		}
+	}
+}
